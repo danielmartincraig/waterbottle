223cef77020ddcf8d73cb9932201547df244b69f
danielmartincraig <danielmartincraig@gmail.com>
2023-05-05 10:03:00 -0500

    now generates part list as a lazy seq

--- src/com/danielmartincraig/waterbottle.clj
+++ src/com/danielmartincraig/waterbottle.clj
@@ -3,12 +3,19 @@
   (:require [com.danielmartincraig.lang :as wb]
             [clojure.string :as str :refer [join]]))
 
-(def parts
-  [(wb/ldraw-command 3005 1 [0 0 0] [1 0 0 0])
-   (wb/ldraw-command 3065 1 [50 0 0] [1 1 0 0])
-   (wb/ldraw-command 3065 1 [50 0 0] [1 1 -1 0])
-   (wb/ldraw-command 3065 1 [50 0 0] [1 1 1 0])
-   ])
+(def fib-seq
+  ((fn rfib [a b]
+     (lazy-seq (cons a (rfib b (+ a b)))))
+   0 1))
+
+
+(def part-attrs (partition 7 1 fib-seq))
+
+(def part-seq (map (fn [[x y z r i j k]] (wb/ldraw-command 3005 1 [x y z] [r i j k])) part-attrs)) 
+
+(take 2 part-seq)
+
+(def parts (take 30 part-seq))
 
 (defn write-file
   [filename parts & _]
